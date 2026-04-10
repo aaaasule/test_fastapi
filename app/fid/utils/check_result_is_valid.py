bc9f5964259eb62cfebe80ccd330ab99dd006f6d
aaaasule <43957933+aaaasule@users.noreply.github.com>
2026-04-10 09:42:17 +0800

    Add file

---
+++ app/fid/utils/check_result_is_valid.py
@@ -0,0 +1,12 @@
+def check_result_valid(result):
+    # if 'operation' in result:
+    #     if result['operation'] == 'add' and result['id'] != None:
+    #         return False
+    #     if result['operation'] == 'delete':
+    #         if 'field_id' in result and (result['id'] == None or result['field_id'] == None):
+    #             return False
+
+    #         if 'subsystem_id' in result and (result['id'] == None or result['subsystem_id'] == None):
+    #             return False
+                
+    return True
