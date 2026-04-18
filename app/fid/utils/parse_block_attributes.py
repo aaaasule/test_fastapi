5407ceb23e14bdbc4a634184824de066d5066431
aaaasule <43957933+aaaasule@users.noreply.github.com>
2026-04-18 16:46:16 +0800

    Update parse_block_attributes.py

--- app/fid/utils/parse_block_attributes.py
+++ app/fid/utils/parse_block_attributes.py
@@ -1,5 +1,6 @@
 import json
 import sys
+#from app.fid.utils.check_device import check_which_device
 from pathlib import Path
 current_file = Path(__file__).resolve()
 root_dir = current_file.parent
@@ -20,6 +21,56 @@ from app.fid.utils.check_device import check_which_device
 import traceback
 from typing import List, Any, Dict
 import re
+
+
+# def check_which_device(equipment: Dict[str, Any], filename: Any):
+#     for _tail in ['PA', 'PD', 'PE', 'PV', 'PP', 'PW']:
+#         if f'FID.{_tail}' in filename:
+#             return 'TAKEOFF'
+
+#     for _tail in ['PC']:
+#         if f'FID.{_tail}' in filename:
+#             return 'VMB_CHEMICAL'  # PC只有chemical  PS只有gasname
+#     for _tail in ['PS']:
+#         if f'FID.{_tail}' in filename:
+#             return 'VMB_GASNAME'
+
+#     for _tail in ['PB']:
+#         if f'FID.{_tail}' in filename:
+#             if 'INTERFACE_CODE' in equipment:
+#                 return 'TAKEOFF'
+#             else:
+#                 return 'VMB_GASNAME'  # PB只有gasname
+
+#     for _tail in ['ES']:
+#         if f'FID.{_tail}' in filename:
+#             if equipment.get('ID_SHORT'):
+#                 # print(f"ID_SHORT-{equipment.get('ID_SHORT')}")
+#                 if 'GPB' in equipment.get('ID_SHORT').upper():
+#                     return 'GPB'
+#                 elif 'LINE' in equipment.get('ID_SHORT').upper():
+#                     return 'I_LINE'
+#                 else:
+#                     return 'NEW_INTER_'
+
+#     if 'INTERFACE_CODE' in equipment:
+#         return 'TAKEOFF'
+#     elif 'VMB-TYPE' in equipment:
+#         if 'CHEMICALNAME' in equipment:
+#             return 'VMB_CHEMICAL'
+#         elif 'GASNAME' in equipment:
+#             return 'VMB_GASNAME'
+
+#     elif equipment.get('ID_SHORT'):
+#         if 'GPB' in equipment.get('ID_SHORT', '').upper():
+#             return 'GPB'
+#         elif 'LINE' in equipment.get('ID_SHORT', '').upper():
+#             return 'I_LINE'
+#         elif equipment.get('ID', '').upper().startswith('BUS'):
+#             return 'NEW_INTER_'
+
+#     return 'I_LINE'
+
 
 def parse_interface_code(code: str) -> dict:
     if not isinstance(code, str):
@@ -243,7 +294,7 @@ def parse_block_attributes(equipment, filename):
                     "interface_code": f"{sub_system};{building_level};{equipment.get('ID_SHORT') or ''};{IDx}" if IDx not in [None, ''] else f"{sub_system};{building_level};{equipment.get('ID_SHORT') or ''};{_id}",
                     'id': f"{IDx}" if IDx != None else f"{_id}",
                     "field_code": f"{sub_system}.{building_level}.{equipment.get('ID_SHORT')}",
-                    "search_id": equipment.get('ID_SHORT') or '',
+                    "search_id": f"{sub_system};{building_level};{equipment.get('ID_SHORT') or ''};{IDx}" if IDx not in [None, ''] else f"{sub_system};{building_level};{equipment.get('ID_SHORT') or ''};{_id}",
                     "connection_size": equipment.get(f'CS.{_id}', ''),
                     "connection_type": equipment.get(f'CT.{_id}', ''),
                     "equipment_code": equipment.get(f'EQU.{_id}', ''),
