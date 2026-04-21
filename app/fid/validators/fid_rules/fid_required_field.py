d1984745836d868d7e22478ae9755adf3fa1723a
aaaasule <43957933+aaaasule@users.noreply.github.com>
2026-04-21 15:41:57 +0800

    Update fid_required_field.py

--- app/fid/validators/fid_rules/fid_required_field.py
+++ app/fid/validators/fid_rules/fid_required_field.py
@@ -9,7 +9,8 @@ sys.path.insert(0, str(project_root))
 
 from app.fid.validators.base_rules import BaseRule
 from app.fid.models import CheckResult
-#from app.config.fid_config import FID_REQUIRED_FIELDS
+
+# from app.config.fid_config import FID_REQUIRED_FIELDS
 current_file = Path(__file__).resolve()
 root_dir = current_file.parent
 while root_dir.name != 'app' and root_dir.parent != root_dir:
@@ -27,51 +28,54 @@ if str(project_root) not in sys.path:
 from app.config.fid_config import FID_REQUIRED_FIELDS
 
 from app.fid.utils.check_device import check_which_device
+from app.fid.utils.parse_block_attributes import parse_block_attributes
 
 
 class FidRequiredFieldRule(BaseRule):
-
     eqp_type = 'TAKEOFF'
     rule_type = "error"
     rule_name = "必填项缺失"
 
-    def check(self, equipments: Dict[str, List[Dict[str, Any]]], device: str = None, request_data = None) -> List[CheckResult]:
+    def check(self, equipments: Dict[str, List[Dict[str, Any]]], device: str = None, request_data=None) -> List[
+        CheckResult]:
         results = []
 
         if device != None:
             equipments = equipments[device]
 
         for eq in equipments:
-            #print(device, eq)
+            # print(device, eq)
             missing = []
             empty = []
 
             attrs = eq
             device = check_which_device(eq, request_data['filename'])
 
-            #required_fields =
+            # required_fields =
             for field in FID_REQUIRED_FIELDS[device]:
-                if field.upper().startswith(('CHEMICALNAME', 'GASNAME')) and request_data['fab']['name'].endswith(('1','2', '3')):
+                if field.upper().startswith(('CHEMICALNAME', 'GASNAME')) and request_data['fab']['name'].endswith(
+                        ('1', '2', '3')):
                     continue
-                if field.upper().startswith('CS') and request_data['fab']['name'].endswith(('1','2', '3')) and 'ES' in request_data['filename']:
+                if field.upper().startswith('CS') and request_data['fab']['name'].endswith(('1', '2')) and 'ES' in \
+                        request_data['filename']:
                     continue
 
-                #print(f"{device} {field=}")
+                # print(f"{device} {field=}")
                 tmp_result = []
                 field_keys = []
                 for k in attrs:
-                    #print(f"37{k=}")
+                    # print(f"37{k=}")
                     if '.' in field and k.upper().startswith(field):
-                        #print(f'39 {field}')
+                        # print(f'39 {field}')
                         field_keys.append(k)
                     elif '.' not in field and k.upper() == field:
                         field_keys.append(k)
-                        #print(f'43 {field}')
-                #print(f"{field_keys=}")
-                #continue
+                        # print(f'43 {field}')
+                # print(f"{field_keys=}")
+                # continue
 
                 for _key in field_keys:
-                    #value = getattr(eq, _key.lower(), None)
+                    # value = getattr(eq, _key.lower(), None)
                     value = eq.get(_key)
                     value = value.strip() if isinstance(value, str) else value
 
@@ -84,10 +88,8 @@ class FidRequiredFieldRule(BaseRule):
                     if value == "":
                         empty.append(_key)
 
-
                 if len(field_keys) == 0:
-
-                    #eq['detail'] =
+                    # eq['detail'] =
                     results.append(CheckResult(
                         type=self.rule_type,
                         name="关键属性缺失",
@@ -100,8 +102,8 @@ class FidRequiredFieldRule(BaseRule):
                     print(f"{device=} 未存在必填字段{field=} {field_keys=}")
                     print(f"丢失关键业务属性field_keys eq = {eq}")
 
-            #print(f"{missing=}")
-            #print(f"{empty=}")
+            # print(f"{missing=}")
+            # print(f"{empty=}")
 
             if missing:
                 results.append(CheckResult(
@@ -113,7 +115,6 @@ class FidRequiredFieldRule(BaseRule):
                     device=device
                 ))
                 print(f"丢失关键业务属性missing eq = {eq}")
-
             if empty:
                 results.append(CheckResult(
                     type=self.rule_type,
@@ -123,8 +124,31 @@ class FidRequiredFieldRule(BaseRule):
                     equipment=[eq],
                     device=device
                 ))
-                #print(eq)
-        #print(f"{results=}")
+
+
+            # equipments_infos = parse_block_attributes(eq, request_data['filename'])
+            # for equipments_info in equipments_infos:
+            #     for e in empty:
+            #         if e.upper().startswith('ID.') or e.upper().startswith('CS.') or e.upper().startswith('CT.'):
+            #             results.append(CheckResult(
+            #                 type=self.rule_type,
+            #                 name="必填项缺失",
+            #                 description=f"必填项未填写：{', '.join(empty)}",
+            #                 detail=f"必填项未填写：{', '.join(empty)}",
+            #                 equipment=[eq, equipments_info],
+            #                 device=device
+            #             ))
+            #         else:
+            #             results.append(CheckResult(
+            #                 type=self.rule_type,
+            #                 name="必填项缺失",
+            #                 description=f"必填项未填写：{', '.join(empty)}",
+            #                 detail=f"必填项未填写：{', '.join(empty)}",
+            #                 equipment=[eq, equipments_info],
+            #                 device=device
+            #             ))
+                # print(eq)
+        # print(f"{results=}")
 
         return results
 
