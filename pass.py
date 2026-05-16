15f24d61df1aa1ce67ce31194411949e6dc6fa71
aaaasule <43957933+aaaasule@users.noreply.github.com>
2026-05-16 16:35:17 +0800

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,23 +1,9 @@
-2026-05-16 15:24:46 - backen-fastapi - INFO - ------------------------------2026-05-16 15:24:46.314978接收FID图纸数据回填参数------------------------------
-2026-05-16 15:24:46 - backen-fastapi - INFO - ------------------------------2026-05-16 15:24:46.314978接收FID图纸数据回填参数------------------------------
-by_uni_code:{}
-by_uni_code:{}
-takeoff_group_map:{'D2021;FAB2F3;2A27Y-N;12': {'12-S2': 'BBONA01'}}
-by_uni_code:{}
-takeoff_group_map:{'D2021;FAB2F3;2A27Y-N;12': {'12-S2': 'BBONA01'}}
-equipment_code:
-interface_code:
-equipment_code:
-interface_code:
-id_code:D2021;FAB2F3;2a27Y-N;12
-TION_RATIO': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18003), 'A.FLOW_UNIT': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18004), 'A.DESIGN_FLOW': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18005), 'ID.D': <class 'ez
-dxf.entities.attrib.Attrib'> ATTRIB(#18006), 'EQU.D': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18007), 'CS.D': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18008), 'CT.D': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1
-8009), 'ID.C': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1800A), 'EQU.C': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1800B), 'CS.C': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1800C), 'CT.C': <class 'ezdxf.entities.
-attrib.Attrib'> ATTRIB(#1800D), 'ID.B': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1800E), 'EQU.B': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#1800F), 'CS.B': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18010), 'CT.B'
-: <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18011), 'ID': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18012), 'ID.A': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18013), 'EQU.A': <class 'ezdxf.entities.attrib.Attrib'>
- ATTRIB(#18014), 'CS.A': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18015), 'CT.A': <class 'ezdxf.entities.attrib.Attrib'> ATTRIB(#18016)}
-attrs_plain:{'D.UTILIZATION_RATIO': '', 'D.FLOW_UNIT': '', 'D.DESIGN_FLOW': '', 'C.UTILIZATION_RATIO': '', 'C.FLOW_UNIT': '', 'C.DESIGN_FLOW': '', 'B.UTILIZATION_RATIO': '', 'B.FLOW_UNIT': '', 'B.DESIGN_FLOW': '', 'A.UTILIZATION
-_RATIO': '', 'A.FLOW_UNIT': '', 'A.DESIGN_FLOW': '', 'ID.D': 'S1', 'EQU.D': '', 'CS.D': '3/8"', 'CT.D': 'Ft', 'ID.C': 'S2', 'EQU.C': '', 'CS.C': '3/8"', 'CT.C': 'Ft', 'ID.B': 'R1', 'EQU.B': '', 'CS.B': '1/4"', 'CT.B': 'Ft', 'ID'
-: 'D2021;FAB2F3;2a27Y-N;12', 'ID.A': 'R2', 'EQU.A': '', 'CS.A': '1/4"', 'CT.A': 'Ft'}
-block_type:VMB_CHEMICAL
-matched_item:None
+        if id_code == "D2021;FAB2F3;2a27Y-N;12":
+            print(f"equipment_code:{equipment_code}")
+            print(f"interface_code:{interface_code}")
+            print(f"id_code:{id_code}")
+            print(f"attrs_by_tag:{attrs_by_tag}")
+            print(f"attrs_plain:{attrs_plain}")
+            print(f"block_type:{block_type}")
+            print(f"matched_item:{matched_item}")
+            print(f"dxf_filename:{dxf_filename}")
